02460c1bb13801104d55db8eba8eb37d85ec690b
DD0S <87689669+AureusBp2@users.noreply.github.com>
2022-05-23 22:28:16 -0400

    Create Stock.lua

---
+++ Stock.lua
@@ -0,0 +1,61 @@
+-- only for starter supplies
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Averymoistboi/Gasstationman/main/instaplace'),true))()
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Averymoistboi/instaplaceBG/main/bocugs'),true))()
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Carrots",
+    [3] = CFrame.new(-12.883910179138184, 2.4499998092651367, -6.6371660232543945, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Carrots",
+    [3] = CFrame.new(-12.883910179138184, 2.4499998092651367, -5.856322765350342, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Tomato",
+    [3] = CFrame.new(-12.689931869506836, 2.4499998092651367, -7.167461395263672,1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Tomato",
+    [3] = CFrame.new(-13.159929275512695, 2.4499998092651367, -7.167461395263672,1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Cucumber",
+    [3] = CFrame.new(-12.708477020263672, 2.4499998092651367, -7.966507911682129,1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Cucumber",
+    [3] = CFrame.new(-12.708477020263672, 2.4499998092651367, -7.587150573730469, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
+
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Potato",
+    [3] = CFrame.new(-12.79626178741455, 2.4499998092651367, -8.472007751464844,1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
+local args = {
+    [1] = "PlaceProduct",
+    [2] = "Potato",
+    [3] = CFrame.new(-13.149262428283691, 2.4499998092651367, -8.472007751464844,1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage").Remote:FireServer(unpack(args))
